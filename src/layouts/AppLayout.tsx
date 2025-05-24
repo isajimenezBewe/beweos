1e57c9b989802555033e3f81b96dd11c9d65fec3
isajimenez <isabela.jimenez@bewe.io>
2025-05-23 23:34:28 -0500

    removing linter error for build

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
-import { Button } from '@heroui/button';
 import { motion } from 'framer-motion';
 
 interface NavItem {
